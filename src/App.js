bc902aadd3b70429f9d9d9c8d02b4a07d22e06ce
Sean Sáez Fuller <sean.saez.f@gmail.com>
2024-09-23 13:01:50 +0200

    setState meaning

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: { firstName: "Sean", lastName: "Sáez" },
+      company: "TRX",
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}
+          </p>
+          <button
+            onClick={() => {
+              this.setState(() => {
+                return {
+                  name: { firstName: "Liam", lastName: "Sáez" },
+                };
+              });
+            }}
+          >
+            Change Name
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
